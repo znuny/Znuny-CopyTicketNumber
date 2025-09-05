fe39be08b129c1f1b08c2fd05de4ae4e30797c9d
Johannes Nickel <jn@znuny.com>
2025-09-05 07:29:24 +0000

    Initial development.

--- Kernel/Language/de_ZnunyCopyTicketNumber.pm
+++ Kernel/Language/de_ZnunyCopyTicketNumber.pm
@@ -16,20 +16,18 @@ use utf8;
 sub Data {
     my $Self = shift;
 
-    # Custom language strings for ZnunyCopyTicketNumber
-    $Self->{Translation}->{'Copy ticket information'} = 'Ticket-Informationen kopieren';
-    $Self->{Translation}->{'Copy Number'} = 'Nummer kopieren';
-    $Self->{Translation}->{'Copy Number + Title'} = 'Nummer + Titel kopieren';
-    $Self->{Translation}->{'Copied to clipboard!'} = 'In Zwischenablage kopiert!';
-
+    $Self->{Translation}->{'Copy ticket information'}    = 'Ticket-Informationen kopieren';
+    $Self->{Translation}->{'Copy Ticket Number'}         = 'Ticketnummer kopieren';
+    $Self->{Translation}->{'Copy Ticket Number + Title'} = 'Ticketnummer + Titel kopieren';
+    $Self->{Translation}->{'Copied to clipboard!'}       = 'In Zwischenablage kopiert!';
 
     $Self->{JavaScriptStrings} //= [];
     push @{$Self->{JavaScriptStrings}},(
         'Copy ticket information',
-        'Copy Number',
-        'Copy Number + Title',
+        'Copy Ticket Number',
+        'Copy Ticket Number + Title',
         'Copied to clipboard!',
-    )
+    );
     return 1;
 }
 
---
+++ doc/en/feature.md
@@ -0,0 +1,3 @@
+# Functionality
+
+This package adds a copy icon in front of the ticket number in the ticket details view, allowing agents to easily copy either the ticket number or the ticket number with the title to the clipboard.
